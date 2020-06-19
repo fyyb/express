2412335cd29435e655d16970fa616917d5aa48e2
Joao Netto <jnetto@fyyb.com.br>
2020-06-19 18:05:52 -0300

    test folder name changed to example

--- example/index.php
+++ example/index.php
@@ -3,27 +3,26 @@
 require __DIR__ . "./settings.php";
 require __DIR__ . "./vendor/autoload.php";
 
-use Fyyb\Router;
 use Fyyb\Request;
 use Fyyb\Response;
+use Fyyb\Router;
 
 $app = Router::getInstance();
 
-require __DIR__ ."./TesteRouterGroups.php";
-require __DIR__ ."./TesteRouterGroup.php";
-require __DIR__ ."./TesteRouterSimples.php";
+// require __DIR__ ."./TesteRouterGroups.php";
+// require __DIR__ ."./TesteRouterGroup.php";
+require __DIR__ . "./TesteRouterSimples.php";
 
 $app->use('/use', './App/Routes/TesteRouterGroups.php');
 $app->use('/use', './App/Routes/TesteRouterGroup.php');
 $app->use('/use', './App/Routes/TesteRouterSimples.php');
 
-
 /**
  * Parametros pessados com Route Params
  */
-$app->any('/params/route/:param1[/:param2]', function(Request $req, Response $res) {
+$app->any('/params/route/:param1[/:param2]', function (Request $req, Response $res) {
     echo '<pre>';
-    print_r($req->getParams()); 
+    print_r($req->getParams());
     exit;
 });
 
@@ -32,28 +31,27 @@ $app->any('/params/route/:param1[/:param2]', function(Request $req, Response $re
  * OU
  * Parametros pessados com Multipart Form via POST
  */
-$app->any('/params/body', function(Request $req, Response $res) {
+$app->any('/params/body', function (Request $req, Response $res) {
     echo '<pre>';
-    print_r($req->getParsedBody()); 
-    exit;
-});
-
-
-/**
- * Parametros pessados com Query String
- */
-$app->any('/params/qs', function(Request $req, Response $res) {
-    echo '<pre>';
-    print_r($req->getQueryString()); 
+    print_r($req->getParsedBody());
     exit;
 });
 
 /**
  * Parametros pessados com Query String
  */
-$app->any('/params/file', function(Request $req, Response $res) {
+$app->any('/params/qs', function (Request $req, Response $res) {
     echo '<pre>';
-    print_r($req->getUploadedFiles()); 
+    print_r($req->getQueryString());
+    exit;
+});
+
+/**
+ * Parametros pessados com Query String
+ */
+$app->any('/params/file', function (Request $req, Response $res) {
+    echo '<pre>';
+    print_r($req->getUploadedFiles());
     exit;
 });
 
---
+++ example/settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/************************************
+ * Environment settings
+ ***********************************/
+
+// Development Environment
+const ENVIRONMENT = 'dev';
+const BASE_DIR = 'fyyb/example/';
+
+// Production Environment
+// const ENVIRONMENT = 'prod';
